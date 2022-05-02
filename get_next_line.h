154668796e3183179748994ed234000c4893dbbe
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-05-02 11:16:44 -0400

    First header prototype

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chughes <marvin@42quebec.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 10:43:12 by chughes           #+#    #+#             */
+/*   Updated: 2022/05/02 10:43:14 by chughes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
