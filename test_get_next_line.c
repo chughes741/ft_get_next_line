d23afc72f1bc9bb8eca9ef4f45c415420c2180d0
Cole - MBA <coltenhughes@gmail.com>
2022-06-04 15:39:49 -0400

    Modified tester

--- test_get_next_line.c
+++ test_get_next_line.c
@@ -5,13 +5,13 @@
 
 
 int main() {
-	int fd;
-	char* line;
+	int		fd;
+	char*	line;
 
 	fd = open("./test.txt", O_RDONLY);
-	// write(1, "...\n", 4);
+	write(1, "...\n", 4);
 	line = get_next_line(fd);
-	// printf("%s", line);
+	printf("%s", line);
 	free(line);
 	close(fd);
 
