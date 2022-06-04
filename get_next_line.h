21fa7682c9b455d6ad7a0d7695b58955dc315968
Cole - MBA <coltenhughes@gmail.com>
2022-06-04 14:28:43 -0400

    Removed struct from header

--- get_next_line.h
+++ get_next_line.h
@@ -21,10 +21,11 @@ char	*get_next_line(int fd);
 void	*ft_calloc(size_t nitems, size_t size);
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *str, int c);
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoinfree(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_linelen(const char *str);
 int		ft_strichr(const char *str, int c);
+char	*ft_strdup(const char *src);
 
 #endif
